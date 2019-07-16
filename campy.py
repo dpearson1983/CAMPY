41280c798e9fda0c73b9dfa29b268331608fbf55
David Pearson <dpearson@localhost.localdomain>
2019-07-16 13:44:19 -0500

    Updates for Python 3

--- campy.py
+++ campy.py
@@ -1,24 +1,23 @@
 import sys, platform, os
 import numpy as np
 import camb
+import yaml
 from camb import model, initialpower
-from yaml import load, dump
-from astropy.io import ascii
+#from astropy.io import ascii    
 
-try:
-    from yaml import CLoader as Loader, CDumper as Dumper
-except ImportError:
-    from yaml import Loader, Dumper
-    
+print("Parsing parameters...")
+parFile = open(sys.argv[1], 'r')
+inputPars = yaml.load(parFile)
 
-parFile = file(sys.argv[1])
-inputPars = load(parFile, Loader=Loader)
+print("Setting up cosmology...")
 pars = camb.CAMBparams()
 pars.set_cosmology(H0=inputPars['H_0'], ombh2=inputPars['Omega_bh2'], omch2=inputPars['Omega_ch2'],
                    omk=inputPars['Omega_k'], tau=inputPars['tau'])
 
+print("Calculating...")
 results = camb.get_results(pars)
 
+print("Getting non-linear power spectrum...")
 pars.set_dark_energy()
 pars.set_matter_power(redshifts=[inputPars['z']], kmax=inputPars['k_max'])
 pars.InitPower.set_params(ns=inputPars['n_s'])
@@ -27,18 +26,23 @@ results.calc_power_spectra(pars)
 kh, z, pk = results.get_matter_power_spectrum(minkh=inputPars['k_min'], maxkh=inputPars['k_max'], 
                                               npoints=inputPars['num_points'])
 sigma_8 = np.array(results.get_sigma8())
-print sigma_8
+print (sigma_8)
 
-ascii.write([kh, pk[0]], inputPars['outFile'], names=['kh', 'P(k)'], overwrite=True)
+print("Outputting non-linear power spectrum...")
+#ascii.write([kh, pk[0]], inputPars['outFile'], names=['kh', 'P(k)'], overwrite=True)
+np.savetxt(inputPars['outFile'], [kh, pk[0]])
 
+print("Getting non-linear power spectrum...")
 pars.NonLinear = model.NonLinear_none
 results.calc_power_spectra(pars)
 kh_lin, z, pk_lin = results.get_matter_power_spectrum(minkh=inputPars['k_min'], maxkh=inputPars['k_max'], 
                                               npoints=inputPars['num_points'])
 
-ascii.write([kh_lin, pk_lin[0]], inputPars['outLinFile'], names=['kh', 'P(k)'], overwrite=True)
+print("Outputting non-linear power spectrum...")
+#ascii.write([kh_lin, pk_lin[0]], inputPars['outLinFile'], names=['kh', 'P(k)'], overwrite=True)
+np.savetxt(inputPars['outLinFile'], [kh_lin, pk_lin[0]])
 
 sigma_8 = np.array(results.get_sigma8())
-print sigma_8
+print (sigma_8)
 
 
